52252fff5393d4f858a400836ed3d11269452861
Lewenei Aron <leweneiaron@gmail.com>
2019-07-21 17:20:13 +0300

    res.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("my name is lewenei aron!"));
+app.get("/", (req, res) => res.json({ message: "my name is lewenei aron!" }));
 
 app.listen(9000, () => console.log("App listening on port 9000"));
