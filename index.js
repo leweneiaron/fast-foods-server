13b994b8383ba9ad2affc9c3303c80fb3ce6210d
Lewenei Aron <leweneiaron@gmail.com>
2019-07-21 21:15:06 +0300

    connecting to database

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "foo",
+    password: "password",
+    database: "fast_foods"
+});
+          
 
-app.get("/", (req, res) => res.json({ message: "my name is lewenei aron!" }));
+app.get("/api/food", (req, res) => {
+    pool.query("SELECT id, name FROM food", (error, rows) => {
+        if (error) {
+             return res.status(500).json({ error });
+        }
 
+         res.json(rows);
+    });
+});
+        
 app.listen(9000, () => console.log("App listening on port 9000"));
