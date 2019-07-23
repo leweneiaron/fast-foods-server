e294fc337eabbb2ddffe21c5cbe2d5b765ec64e3
Lewenei Aron <leweneiaron@gmail.com>
2019-07-23 18:56:58 +0300

    get.foo,shop

--- index.js
+++ index.js
@@ -19,5 +19,45 @@ app.get("/api/shop", (req, res) => {
          res.json(rows);
     });
 });
+
+
+app.get("/api/shop/:id", (req, res) => {
+         pool.query(
+             "SELECT id, name FROM shop WHERE id = ?",
+             [req.params.id],
+             (error, rows) => {
+                if (error) {
+                     return res.status(500).json({ error });
+                 }
+    
+                 res.json(rows);
+             }
+         );
+     });
+    
+     app.get("/api/food", (req, res) => {
+        pool.query("SELECT id, name FROM food", (error, rows) => {
+            if (error) {
+                 return res.status(500).json({ error });
+            }
+    
+             res.json(rows);
+        });
+    });
+
+
+    app.get("/api/food/:id", (req, res) => {
+        pool.query(
+            "SELECT id, name FROM food WHERE id = ?",
+            [req.params.id],
+            (error, rows) => {
+               if (error) {
+                    return res.status(500).json({ error });
+                }
+   
+                res.json(rows);
+            }
+        );
+    });
         
 app.listen(9000, () => console.log("App listening on port 9000"));
